3420a1b7de2fae277b955915b660444b56490114
prajjwalsilwal <142755665+prajjwalsilwal@users.noreply.github.com>
2024-10-24 14:45:30 -0500

    Add files via upload

---
+++ challenge20.py
@@ -0,0 +1,7 @@
+start_num = int(input("Starting number: "))
+end_num = int(input("Ending number: "))
+increment_val = input("Increment by: ")
+
+for i in range(start_num,end_num,int(increment_val)):
+    print(i)
+
